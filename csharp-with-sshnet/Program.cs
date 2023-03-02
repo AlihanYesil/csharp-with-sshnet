f6fef01992b2d97c53a0bd37f3cce4cffc37b58a
Alihan Yesil <alihannyesil@gmail.com>
2023-03-02 10:21:33 +0300

    Initial commit

---
+++ csharp-with-sshnet/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Renci.SshNet;
+
+var sshClient = new SshClient("IP", "Username", "password");
+sshClient.Connect();
+var command = sshClient.CreateCommand("ls");
+
+var result = command.Execute();
+Console.WriteLine(result);
+
